cdf76e7206b9324ae889dd3afa3aa47aeba32e4f
Franklin Cortés <andres10cortes07@gmail.com>
2024-05-28 13:00:37 -0500

    query random for get all processors

--- models/processors.mjs
+++ models/processors.mjs
@@ -7,7 +7,7 @@ export class ModelsProcessors {
                 SELECT BIN_TO_UUID(pro.id) AS id, pro.marca, pro.modelo, pro.numNucleos, pro.numHilos, pro.relojBase, 
                 pro.precio, r.url1, r.url2, r.url3, r.url4, r.url5, r.url6 
                 
-                FROM procesadores pro INNER JOIN recursos r ON pro.recursos_id_recurso = r.id_recurso ;
+                FROM procesadores pro INNER JOIN recursos r ON pro.recursos_id_recurso = r.id_recurso ORDER BY RAND();
             `
         );
 
